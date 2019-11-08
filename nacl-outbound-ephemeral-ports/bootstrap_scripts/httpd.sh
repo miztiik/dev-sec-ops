37c058782f9de5b7b23eb33e8c77563e1e7da8d0
Mystique <13oct08@quantumfoam.uni.cc>
2019-11-08 11:50:29 +0100

    Updates to templates

--- nacl-outbound-ephemeral-ports/bootstrap_scripts/httpd.sh
+++ nacl-outbound-ephemeral-ports/bootstrap_scripts/httpd.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
+#!/bin/bash -xe
+
+# Lets log everything to console for being lazy (not recommended)
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 #install httpd
-yum install httpd -y;
+yum install httpd ec2-instance-connect -y
 
 #enable and start httpd
-systemctl enable httpd;
-systemctl start httpd;
-echo "<html><head><title> Mystique Web Server</title></head>" >  /var/www/html/index.html;
-echo "<body>" >>  /var/www/html/index.html;
-echo "<div><center><h2>Welcome AWS $(hostname -f) </h2>" >>  /var/www/html/index.html;
-echo "<hr/>" >>  /var/www/html/index.html;
-curl http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html;
-echo "</center></div></body></html>" >>  /var/www/html/index.html;
+systemctl enable httpd
+systemctl start httpd
+echo "<html><head><title> Mystikal World</title></head>" >  /var/www/html/index.html
+echo "<body><br/><br/><br/><br/><br/><hr/>" >>  /var/www/html/index.html
+echo "<div><center><h2>Welcome to Valaxy </h2>" >>  /var/www/html/index.html
+echo "<hr/>" >>  /var/www/html/index.html
+echo "<div><center><h5> $(curl http://169.254.169.254/latest/meta-data/instance-id) - $(hostname -f), $(date)</h5>" >> /var/www/html/index.html
+echo "</center></div></body></html>" >>  /var/www/html/index.html
